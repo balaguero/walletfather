5e24cfe70e0348cacc56e21a9e57710df812d153
Juan Balaguero <juan.server2004@gmail.com>
2022-01-18 11:57:33 -0300

    Fix minor typo in dashboard-web3 module

--- frontend/src/app/modules/dashboard-web3/dashboard-web3.component.ts
+++ frontend/src/app/modules/dashboard-web3/dashboard-web3.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard-web3',
   templateUrl: './dashboard-web3.component.html',
-  styleUrls: ['./dashboard-web.component.scss']
+  styleUrls: ['./dashboard-web3.component.scss']
 })
 export class DashboardWeb3Component implements OnInit {
 
