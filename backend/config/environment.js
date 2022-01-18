0cc01e28b1ec51db26731a18c91a0c6e8c5bade4
Juan Balaguero <juan.server2004@gmail.com>
2022-01-18 13:30:18 -0300

    update contract address

--- backend/config/environment.js
+++ backend/config/environment.js
@@ -5,12 +5,12 @@
 
 module.exports = {
     production: {
-        contractAddress: '0x27bFfC6459Da9E3135bACD9E84F7feFdbaf1EF2a',
+        contractAddress: '0x91B47a1ff0131fE41f4aF50e492139821b5B0D38',
         authTokenSecret: '4b8da58f53a989d92a98f0fd3ea9617a9ed65c65b1dad1902f475d312a540410',
         mongoConnectionString: 'mongodb+srv://unmineropichi:GWVNGildOIX5ObS6@cluster0.mysa9.mongodb.net/walletfather?retryWrites=true&w=majority'
     },
     development: {
-        contractAddress: '0x27bFfC6459Da9E3135bACD9E84F7feFdbaf1EF2a',
+        contractAddress: '0x91B47a1ff0131fE41f4aF50e492139821b5B0D38',
         authTokenSecret: '4b8da58f53a989d92a98f0fd3ea9617a9ed65c65b1dad1902f475d312a540410',
         mongoConnectionString: 'mongodb+srv://unmineropichi:GWVNGildOIX5ObS6@cluster0.mysa9.mongodb.net/walletfather?retryWrites=true&w=majority'
     }
